f75cc221523fe98670762cba8b1698620020b9b2
Kesman <kesman0709@gmail.com>
2026-06-05 07:27:09 -0500

    Fix imports in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 sys.path.append("src")
 
 from flask import Flask
-from src.view.Web.liquidacion_routes import blueprint
+from view.Web.liquidacion_routes import blueprint
  
 app = Flask(__name__, template_folder="src/templates")
 app.register_blueprint(blueprint)
